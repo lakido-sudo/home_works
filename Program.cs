ccb2b5c480678961389bcb839ec93a10497e309b
lakido-sudo <119069754+lakido-sudo@users.noreply.github.com>
2022-12-19 19:27:44 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,26 @@
 ﻿//1
-Console.Write("Задача 19");
-Console.Write("Введите пятизначное число: ");
-string? number = Console.ReadLine();
+Console.WriteLine("Задача 25");
+int A = 3;
+int B = 5;
 
-void CheckingNumber(string number)
-{
-  if (number[0]==number[4] || number[1]==number[3])
-  {
-    Console.WriteLine($"{number} - это палиндром.");
-  }
-  else Console.WriteLine($"{number} - это не палиндром.");
-}
-
-if (number!.Length == 5){
-  CheckingNumber(number);
-}
-else Console.WriteLine($"Вы ввели не пятизначное число");
+double powAinB = Math.Pow(A,B);
+Console.WriteLine(powAinB);
 
 //2
-Console.Write("Задача 21");
+Console.WriteLine("Задача 27");
+int C = 452;
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+int sumNumbersInC = 0;
 
-int Coordinate(string coorName, string pointName)
-{
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2)
-{
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-}
-
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-
-Console.WriteLine($"Длина отрезка  {segmentLength}");
+while (C>0) 
+{ 
+sumNumbersInC = sumNumbersInC + (C % 10); 
+C =  C/10; 
+} 
+Console.WriteLine(sumNumbersInC);
 
 //3
-Console.WriteLine("Задача 23");
-
-int n = 3;
-double pow3 = Math.Pow(n,3);
-Console.WriteLine(pow3);
-
+Console.WriteLine("Задача 29");
+int[] array = new int[8]{1,2,3,4,5,6,7,8};
+Array.ForEach(array,Console.WriteLine);
 
