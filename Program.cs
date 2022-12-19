7fdba78e2656037c7e532d3e10cab5164716a8ff
lakido <lakido551@gmail.com>
2022-12-19 18:00:56 +0300

    tasks

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿//1
+Console.Write("Задача 19");
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"{number} - это палиндром.");
+  }
+  else Console.WriteLine($"{number} - это не палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Вы ввели не пятизначное число");
+
+//2
+Console.Write("Задача 21");
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+//3
+Console.WriteLine("Задача 23");
+
+int n = 3;
+double pow3 = Math.Pow(n,3);
+Console.WriteLine(pow3);
+
+
